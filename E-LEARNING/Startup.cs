6ee3222ba18e1563c5bd6c36aa7ef65bd002ecc6
Carlos <carlosluisgonzalez29@gmail.com>
2020-03-28 18:55:38 -0600

    initProyect

---
+++ E-LEARNING/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(E_LEARNING.Startup))]
+namespace E_LEARNING
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
