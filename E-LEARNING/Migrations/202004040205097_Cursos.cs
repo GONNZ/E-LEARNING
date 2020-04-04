354d2bd6e9908b65a35175a0c58d65bc0fb5c71d
Carlos <carlosluisgonzalez29@gmail.com>
2020-04-03 21:27:52 -0600

    Cursos
    CRUD

---
+++ E-LEARNING/Migrations/202004040205097_Cursos.cs
@@ -0,0 +1,16 @@
+namespace E_LEARNING.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Cursos : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
